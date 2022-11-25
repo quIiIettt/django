12d7c7a36d2d4f003abcde712722afa0f37a2594
dqie.ee <103836441+quIiIettt@users.noreply.github.com>
2022-11-25 15:22:03 +0200

    update views.py, models.py

--- virtualenv/Scripts/temperatureSensors/tempSensors/views.py
+++ virtualenv/Scripts/temperatureSensors/tempSensors/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
+from .models import *
 
-# Create your views here.
+
+
+def index(request):
+    sensors = TemperatureSensors.objects.all()
+    return render(request, 'index.html', {'sensors': sensors})
+
+def sensor_page(request, location):
+    try:
+        sensor = TemperatureSensors.objects.get(location = location)
+        sensor = [(str(k).capitalize().replace('_',''), v) for k, v in sensor.__dict__.items()]
+    except ObjectDoesNotExist:
+        return HttpResponse("wrong location")
+    return render(request, 'dja3prac.html', {'server': sensor[1:]})
